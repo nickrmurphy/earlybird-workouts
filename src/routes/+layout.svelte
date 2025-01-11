0605318513fe5afc36d52868996f58b738f26e05
nickrmurphy <hi@nickmurphy.io>
2025-01-10 19:11:34 -0600

    maybe now fr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,4 +18,15 @@
 
 </script>
 
-{@render children?.()}
+<div>
+	{@render children?.()}
+</div>
+
+<style>
+	div {
+		padding-top: env(safe-area-inset-top);
+		padding-bottom: env(safe-area-inset-bottom);
+		padding-right: env(safe-area-inset-right);
+		padding-left: env(safe-area-inset-left);
+	}
+</style>
