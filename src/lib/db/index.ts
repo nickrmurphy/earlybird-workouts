3aaf3dd87cd8679c2fffb035ab78929543084149
nickrmurphy <hi@nickmurphy.io>
2025-03-14 13:14:11 -0500

    fully remove dexie

--- src/lib/db/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./db";
-export * from "./mutations";
-export * from "./queries";
-export * from "./schema";
-export * from "./stateQuery.svelte";
--- src/lib/db/schema/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./exercises";
-export * from "./history";
-export * from "./workouts";
